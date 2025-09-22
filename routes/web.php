c0158dd9fde9989ead97b4978a6e2fd16ac7b252
0xShun <shawnlovecode14@gmail.com>
2025-09-23 00:37:15 +0800

    feat(web): routes for food and places pages

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Frontend pages (Livewire)
+Route::view('/food', 'pages.food')->name('food.index');
+Route::view('/places', 'pages.places')->name('places.index');
